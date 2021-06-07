19d2902280d7d8d2aec3986aecc4b6ece44f2149
Jacob Earle <jacob.earle@yale.edu>
2021-06-07 19:21:32 -0400

    Followed instructions in STM32F4 manual to set up UART, still need to get it to output to qemu

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,37 @@ fn main() -> ! {
 
     // initializing the uart
     
-    let mut peripherals = stm32f407::Peripherals::take().unwrap();
+    let peripherals = stm32f407::Peripherals::take().unwrap();
     let uart = &peripherals.USART1;
 
-    debug::exit(debug::EXIT_SUCCESS);
+    // 1. Enable the USART by writing the UE bit in USART_CR1 register to 1.
+    uart.cr1.modify(|_, w|  w.ue().bit(true));
+
+    // 2. Program the M bit in USART_CR1 to define the word length.
+    // Setting to 0 transmits 8 bit words
+    uart.cr1.modify(|_, w| w.m().bit(false));
+
+    // 3. Program the number of stop bits in USART_CR2.
+    // This is 0 by default, i.e. 1 stop bit, which is fine for our purposes
+    uart.cr2.modify(|_,w| w.stop().bits(0));
+
+    // 4. Select DMA enable (DMAT) in USART_CR3 if Multi buffer Communication is to take place. Configure the DMA register as explained in multibuffer communication.
+    // For now, we will just attempt to send a single character, so we will not bother with DMA
+    uart.cr3.modify(|_,w| w.dmat().bit(false));
+
+    // 5. Select the desired baud rate using the USART_BRR register.
+    // We will aim for a baudrate of 9600
+    // The cpu has a default clock speed of 16 MHz, so the BRR value should be approx. 1666.67, which has integral part 1666 and fractional part approx. 11/16
+    uart.brr.modify(|_,w| w.div_mantissa().bits(1666).div_fraction().bits(11));
+
+    // 6. Set the TE bit in USART_CR1 to send an idle frame as first transmission.
+    uart.cr1.modify(|_,w| w.te().bit(true));
+
+    // 7. Write the data to send in the USART_DR register (this clears the TXE bit). Repeat this for each data to be transmitted in case of single buffer
+    // We will write the character "F" to the uart, represented by ASCII code "0x46"
+    uart.dr.write(|w| w.dr().bits(0x46));
+
+    //debug::exit(debug::EXIT_SUCCESS);
 
 
     loop{
